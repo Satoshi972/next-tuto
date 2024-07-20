4732df050890a89b48c1b165bcf250b74efd4ad7
Ruddy MARIE-LUCE <ruddy.marieluce@gmail.com>
2024-07-20 11:55:40 +0200

    First commit | chap 6

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CustomerPage() {
+  return <div>Customer Page</div>;
+}
+
+export default CustomerPage;
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana, Inika } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+export const inika = Inter({ subsets: ["latin"] });
